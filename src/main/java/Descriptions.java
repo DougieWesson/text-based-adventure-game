d5f6311c7f088eaf4fc1ec4f3c1920fce36bb151
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2022-12-01 16:10:29 +0000

    progress save

--- src/main/java/Descriptions.java
+++ src/main/java/Descriptions.java
@@ -1,32 +1,59 @@
 
 public class Descriptions {
 
+  //General Descriptions
+
+  public static final String BOOT_MESSAGE = "Welcome to Starship Stowaway! I hope you enjoy the game, and type 'help' if you need any guidance with the commands. -DW" +
+          "\nYou awaken in the tiny gloomy room you managed to hideaway in on the IPSS MoonGazer, your nav-card bleeping to signal that this is" +
+          " when you need to jump ship or else you'll miss your destination. Cursing your repeat snoozing, you take stock and resolve to make your way to the" +
+          " flight deck and find a way off this dingy beater of a transport.";
+
+  public static final String HELP = "Here are some examples of commands and how to navigate:" +
+          "\nGO/MOVE and then NORTH/EAST/SOUTH/WEST will move you in that direction." +
+          "\nLOOK/INSPECT and then ROOM or whatever you want to learn more about." +
+          "\nTAKE/GRAB/GET and then the name of the object you'd like to acquire will, uh, do that." +
+          "\nTALK and then the name of the thing you'd like to talk to." +
+          "\nUSE, then the item you'd like to use, and then the thing you'd like to use it on." +
+          "\nI hope this 'helps'. Get it? I'll go now..."
+          ;
+
   //Level One Room Descriptions
-  public static final String BRIG = "you awaken in a tiny gloomy room. you are sat on a filthy bed covered with " +
-          "a coarse blanket, no pillow. \n and a foul smelling bucket for your more primitive moments is on the opposite side " +
-          "of the room, the thick iron door on the east wall has been left ajar,\n you briefly consider giving them a scathing " +
-          "review on trip advisor, but settle on escaping this dingy vessel instead.";
+  public static final String BRIG = "You walk into the brig, a small room on these kind of ships but just big enough to house " +
+          "you for your trip. There's not much in here really, but it's served you just fine.";
   public static final String HALLWAY = "You walk into a hallway, a light flickers from the ceiling proving a dim view " +
-          "of your surroundings, you see the Mona Lisa tracking you from a wall, she creeps you out, and three doorways, one straight " +
-          "ahead of you to the WEST, and one either side of you to the NORTH and SOUTH.";
-  public static final String WORKSHOP = "It smells very flammable.";
-  public static final String LOCKER_ROOM = "This room has lockers in it";
-  public static final String OFFICE = "You see an office, it has a big picture of Dan, eating pastries on one wall, it " +
-          "is both a confusing and inspiring piece. and a chair. theres also a vent on the north wall";
+          "of your surroundings. Being a service hallway in the bowels of a ship, there's still not a lot here, but there" +
+          " are a few bits and bobs lying around and a service bot you mentally called jimbo due to his apparent drawl." +
+          " There are three doorways, one straight ahead of you to the WEST, and one either side of you to the NORTH and SOUTH.";
+  public static final String WORKSHOP = "It smells very flammable. There's too many boxes and drawers to look through, but " +
+          "maybe jimbo knows what is actually useful in here.";
+  public static final String LOCKER_ROOM = "This room has lockers in it. Ever since the ship got refitted for public use, these" +
+          " lockers are reserved for staff only, but since most of the staff are now bots, pickings are slim.";
+  public static final String OFFICE = "You see an office. It's a bare fitted affair, presumably used for keeping staff records " +
+          "and processing brig inhabitants, but the last record you can find on it is dated at least 42 years ago." +
+          " There's also the vent on the north wall that you used to get back here, but jimbo very helpfully " +
+          "'fixed' the covering, screwing it back into place.";
   public static final String VENT = "A functional service vent, it seems a little unstable.";
-  public static final String LEVEL_ONE_SUCCESS = "You have crawled into the vent and make your way to the living quarters of the ship, the vent shaft collapsing as you leave it.";
+  public static final String LEVEL_ONE_SUCCESS = "You have crawled into the vent and make your way to the living quarters of the ship," +
+          " the vent shaft collapsing as you leave it. You make a mental note to find a new hiding spot if you're ever back here, and replace the cover " +
+          "to hide your tracks.";
 
 
   //Level Two Room Descriptions
-  public static final String HALLWAY_TWO = "Hallway flavour space";
+  public static final String HALLWAY_TWO = "A basic hallway, there's some notices on the wall about ship etiquette and cleaning up after your self, " +
+          "but you suspect no one has read them in years.";
 
-  public static final String LOUNGE = "Lounge flavour";
+  public static final String LOUNGE = "This counts as a lounge, but the only thing that counts as furniture is a broken sofa with a vagrant on it." +
+          " The vagrant seems to hopefully be passed out, but finding out might draw attention to you";
 
-  public static final String BUNKS = "A room full of beds.";
+  public static final String BUNKS = "A room full of bunks serving as beds, but they're barely bare metal sheets. A couple of legitimate passengers " +
+          "are using them, but keeping a low profile might be best. One of the passengers eyes you in a way you can't figure out...";
 
-  public static final String KITCHEN = "A barren but functional kitchen";
+  public static final String KITCHEN = "A barren but functional kitchen. The nutrient heaters 'work', but often just make the centre of your food " +
+          "hot and whatever it's on, surrounded by a half-frozen shell. Most people prefer to cook using heating vents. There is also a fridge in the corner, " +
+          "usually used for food.";
 
-  public static final String ACCESSWAY = "A passage to access further into the ship.";
+  public static final String ACCESSWAY = "A passage to access further into the ship. There's a door to the NORTH and stairway to the EAST, but it's " +
+          "locked behind a keycard scanner reserved for authorised personnel.";
 
   public static final String STORAGE = "A storage room, for dust apparently.";
 
@@ -43,18 +70,17 @@ public class Descriptions {
 
   public static final String BATHROOM = "You enter a perfectly normal and clean bathroom.";
 
-  public static final String FOYER = "you bounce into a surprisingly lovely room, your feet are cuddled by a thick fluffy carpet, a chandelier gracefully drifts down from the ceiling, thick velvety cushioned seats surround the room, as well as a delicate cheese platter\n" +
-          " and thick gold plated doors bar entry to what can only be more beautiful rooms.";
+  public static final String FOYER = "you enter into a foyer, a surprisingly nice room for this ship. It has furniture and tables and even has fabric on the floor.";
 
   public static final String FIRST_MATE_ROOM = "you wander into a well furnished room with a four poster princess bed, every corner of the room seems buried in sparkly stuffed unicorns.";
 
-  public static final String HANGER = "you enter a large hanger, theres a desk covered in books and papers, a greasy puddle and most importantly, several ever so slightly rusty escape pods line the northern wall";
+  public static final String HANGER = "you enter a large hanger. There's a desk covered in books and papers, a greasy puddle and most importantly, several ever so slightly " +
+          "rusty escape pods line the northern wall, and a status screen for flight viability and fuel status.";
 
-  public static final String CAPTAIN_ROOM = "you scuttle into the captains room, it's a velvety room, majestic in it's extravagance, filled with audacious self portraits of the captain in horrifyingly extreme erotic poses, several stuffed monstrous animals decorate the room and a huge golden clam\n" +
-          " dominates the far end of the room, it appears to of been the captains bed and is worryingly etched with the words 'pleasure chamber' a large fleshy body lies within the clam\n" +
-          "he has a silk scarf stretched over his neck, and wears nothing but a thick leather belt with a jewel encrusted buckle crafted into a fat seahorse.";
+  public static final String CAPTAIN_ROOM = "you enter into the captains room, or at least it used to be when a captain was actually needed. The once pleasant furnishings has become worn and grey, " +
+          "and faded notes litter the desk.";
 
-  public static final String FUEL_ROOM = "a dank room, containing shelves of fuel, some of which even have fuel in them.";
+  public static final String FUEL_ROOM = "a dank room, containing shelves of fuel canisters, some of which even have fuel in them.";
 
   public static final String ESCAPE_POD = "its a creaky old coffin, with a navigation system that can barely run Doom, despite evidence of multiple attempts to do so, but if it can get you off the ship, you'd blast off in a bin";
 
